d12ef8254ef85025f0651c0d3190c28ed7aea66f
mehelbangin <mehel.bangin@gmail.com>
2023-05-22 19:21:55 +0200

    Dodanie AOS na liste hoteli

--- src/components/Hotels/Hotel/hotel.js
+++ src/components/Hotels/Hotel/hotel.js
@@ -1,10 +1,20 @@
-import React from 'react';
 import styles from './hotel.module.css';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Hotel(){
+    useEffect(() => {
+        AOS.init({
+          duration: 600, // Czas trwania animacji (w milisekundach)
+          easing: 'ease-out', // Wygładzanie animacji
+          once: false, // Czy animacja ma być wywołana tylko raz
+        });
+      }, []);
+
     return (<div className='row'>
         <div className='col-12'>
-            <div className={`${styles.mrgbtm} row`}>
+            <div className={`${styles.mrgbtm} row`} data-aos="fade-up">
                 <div className={`${styles.hotelphoto} col-lg-4`}></div>
                 <div className={`${styles.hotelopis} col-lg-8`}>
                     <h2 className={styles.h2name}>
